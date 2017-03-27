23a2d89e628855371547eb2ce2d5f5b3bf9f992c
felixacdc <pfmata01@gmail.com>
2017-03-27 10:31:20 -0600

    [add] middleware

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 const express = require('express'),
 	port = 8999,
 	app = express(),
-	bodyParser = require('body-parser');
+	bodyParser = require('body-parser'),
+	expressSession = require('express-session'),
+	cookieParser = require('cookie-parser');
 
+function checkAuth (req, res, next) {
+	console.log('checkAuth ' + req.url);
+	if (req.url === '/secure' && (!req.session || !req.session.authenticated)) {
+		res.render('unauthorised', { status: 403 });
+		return;
+	}
+
+	next();
+}
+
+app.use(cookieParser());
+app.use(expressSession({secret:'example'}));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(checkAuth);
 
 app.set('view engine', 'jade');
 app.set('view options', { layout: false });
+
 
 app.listen(port);
 console.log('Node listening on port %s', port);
